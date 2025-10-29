ba0ef0fe420c1cdf5a1db2d3a0a93015bb324d40
Berrich2024 <sylvain.lagonotte.fr@gmail.com>
2025-10-29 15:31:31 +0100

    Update generate-content.js

--- scripts/generate-content.js
+++ scripts/generate-content.js
@@ -3,7 +3,7 @@ import path from "path";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-flash-latest" });
 
 const today = new Date();
 const yyyyMmDd = today.toISOString().split("T")[0];
